b192f41a349bebe919325703b0a504851bc22680
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2021-05-16 23:26:25 +0200

    Remove unnecessary transitions and gsap functions

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,18 @@
 <template>
   <Navbar />
   <router-view v-slot="{ Component }">
-    <transition
-      name="route"
-      mode="out-in"
-      @before-enter="beforeEnter"
-      @enter="enter"
-      @before-leave="beforeLeave"
-      @leave="leave"
-    >
-      <component :is="Component"></component>
-    </transition>
+    <component :is="Component"></component>
   </router-view>
 </template>
 
 <script>
-import gsap from "gsap";
-import Navbar from './components/Navbar.vue';
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
-  components: { Navbar },
-  setup() {
-    const beforeEnter = (el) => {
-      el.style.opacity = 0.5;
-      el.style.transform = "translateX(100px)";
-    };
-    const enter = (el, done) => {
-      gsap.to(el, {
-        opacity: 1,
-        x: 0,
-        duration: 0.5,
-        onComplete: done,
-      });
-    };
-    const beforeLeave = (el) => {
-      el.style.opacity = 1;
-    };
-    const leave = (el, done) => {
-      gsap.to(el, {
-        keyframes: [
-          { opacity: 0, transform: "translateX(-100px)", delay: 0.5 },
-        ],
-        onComplete: done,
-      });
-    };
-    
-    return { beforeEnter, enter, beforeLeave, leave };
-  },
+  components: { Navbar }
 };
 </script>
 
 <style>
-
 </style>
